0d22e292f1da8b472c0c867283d5fdb5f0ee9f50
GalyameevMihail <ruslan28121976@gmail.com>
2023-09-30 01:15:03 +0500

    Task3

---
+++ Tyuiu.GalyameevMR.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GalyameevMR.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GalyameevMR.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7980230-076e-43e5-ba83-b47ecd601833")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GalyameevMR.Sprint0.Task3.V0/Tyuiu.GalyameevMR.Sprint0.Task3.V0.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GalyameevMR.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 8));
+            Console.ReadKey();
+        }
+    }
+}
