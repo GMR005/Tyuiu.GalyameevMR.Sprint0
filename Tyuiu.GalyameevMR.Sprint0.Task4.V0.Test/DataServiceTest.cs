984257d05e970461f5fee9840bd8b61bfd930556
GalyameevMihail <ruslan28121976@gmail.com>
2023-09-30 20:12:21 +0500

    Task4

---
+++ Tyuiu.GalyameevMR.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GalyameevMR.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void SumValid()
+        {
+            Assert.AreEqual(12, DataService.Sum(6, 6));
+        }
+
+        [TestMethod]
+        public void RaznValid()
+        {
+            Assert.AreEqual(0, DataService.Razn(6, 6));
+        }
+
+        [TestMethod]
+        public void ProizValid()
+        {
+            Assert.AreEqual(36, DataService.Proizv(6, 6));
+        }
+
+        [TestMethod]
+        public void ChastnValid()
+        {
+            Assert.AreEqual(1, DataService.Chastn(6, 6));
+        }
+    }
+}
---
+++ Tyuiu.GalyameevMR.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GalyameevMR.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(1, 2));
+            Console.WriteLine(DataService.Razn(6, 3));
+            Console.WriteLine(DataService.Proizv(5, 4));
+            Console.WriteLine(DataService.Chastn(4, 2));
+            Console.ReadKey();
+        }
+    }
+}
