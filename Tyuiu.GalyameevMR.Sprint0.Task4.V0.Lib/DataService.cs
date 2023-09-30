819761d043b523fb755b0e1b5743fc2efe34afd6
GalyameevMihail <ruslan28121976@gmail.com>
2023-09-30 20:16:11 +0500

    Task4

---
+++ Tyuiu.GalyameevMR.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GalyameevMR.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Razn(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Proizv(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Chastn(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
